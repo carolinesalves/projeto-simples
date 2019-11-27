24dea031bc2b70b84d48ce2d64f6e6e7ca8f25d0
Alves <caroline.s.alves@accenture.com>
2019-11-27 09:44:45 -0300

    Soma de dois numeros

---
+++ Exercicio_Python/Exerc3.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite o primeiro numero: "))
+print(num1)
+
+num2 = int(input("Digite o segundo numero: "))
+print(num2)
+
+soma = num1 + num2
+print("A soma dos numeros é: ", soma)
+
+
+
