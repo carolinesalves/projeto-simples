f649dcffe1ed25d8359cb67178a478ff0090ef39
Alves <caroline.s.alves@accenture.com>
2019-12-09 14:51:03 -0300

    Conversão de Medidas

--- Exercicio_Python/Exerc5.py
+++ Exercicio_Python/Exerc5.py
@@ -1,3 +1,8 @@
+# Este codigo é referente ao exercicio 5
+# Criado em: 09/12/2019
+# Criadora: Caroline Salgado Alves
+# Versão: 1.0
+
 medida = float(input("Digite uma medida em cm:  "))
 
 nova = medida/100
