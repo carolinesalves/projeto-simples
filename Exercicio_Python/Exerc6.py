584f458883c1c628c06664895b7b95299947a220
Alves <caroline.s.alves@accenture.com>
2019-12-09 14:56:04 -0300

    area do circulo

--- Exercicio_Python/Exerc6.py
+++ Exercicio_Python/Exerc6.py
@@ -1,3 +1,8 @@
+# Este codigo é referente ao exercicio 5
+# Criado em: 09/12/2019
+# Criadora: Caroline Salgado Alves
+# Versão: 1.0
+
 raio = float(input("Digite o raio do circulo: "))
 
 area = 3.14 * (raio**2)
