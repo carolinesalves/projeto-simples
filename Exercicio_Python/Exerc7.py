a4301bc4cdf2b528ce8a042b8154e19291ff4974
Alves <caroline.s.alves@accenture.com>
2019-12-09 15:14:27 -0300

    area do quadrado

---
+++ Exercicio_Python/Exerc7.py
@@ -0,0 +1,7 @@
+lado = int(input("Digite o tamanho do lado do quadrado: "))
+
+area = lado**2
+print("A área do quadrado é: ", area)
+
+dobro = area*2
+print("O dobro da área do quadrado é: ", dobro)
