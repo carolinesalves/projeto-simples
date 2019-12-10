2247686347ccfd259e854cc3c140b10409442558
Alves <caroline.s.alves@accenture.com>
2019-12-10 10:57:28 -0300

    Operações

---
+++ Exercicio_Python/Exerc11.py
@@ -0,0 +1,13 @@
+N1 = int(input("Digite o primeiro número inteiro: "))
+N2 = int(input("Digite o segundo número inteiro: "))
+R = float(input("Digite um número real: "))
+
+produto = (2 * N1) * (N2/2)
+print("O produto entre os números é: ", round(produto,1))
+
+soma = (3*N1) + R
+print("A soma dos números é: ", round(soma,1))
+
+potencia = R**3
+print("A potência do número é: ", round(potencia,1))
+
